8eddc5b97598ddb4457acf866fad1b770b521835
rei07x <152735564+rei07x@users.noreply.github.com>
2023-12-27 04:02:18 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import configparser
 import qrcode
 
-print(r" _____  _          __          __   _ _    _____      _ ")
-print(r"|  __ \| |         \ \        / /  | | |  |  __ \    (_)")
-print(r"| |__) | |_   _  __ \ \  /\  / /_ _| | | _| |__) |___ _ ")
-print(r"|  ___/| | | | |/ _` \ \/  \/ / _` | | |/ /  _  // _ \ |")
-print(r"| |    | | |_| | (_| |\  /\  / (_| | |   <| | \ \  __/ |")
-print(r"|_|    |_|\__,_|\__, | \/  \/ \__,_|_|_|\_\_|  \_\___|_|")
+ascii_art = r"""
+           
+   ____        _____            
+  / __ \      / ____|           
+ | |  | |_ __| |  __  ___ _ __  
+ | |  | | '__| | |_ |/ _ \ '_ \ 
+ | |__| | |  | |__| |  __/ | | |
+  \___\_\_|   \_____|\___|_| |_|
+                                
+                                
+                                
+"""
+
+cool_message = """
+  💻 Developed By @rei07x 
+  🌐 GitHub: https://github.com/rei07x
+  📡 Telegram: https://t.me/rei07x
+"""
+
+print(ascii_art)
+print(cool_message)
 
 def generate_qr_code(url, file_name="qrcode.png"):
     qr = qrcode.QRCode(
@@ -20,7 +35,7 @@ def generate_qr_code(url, file_name="qrcode.png"):
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(file_name)
 
-def get_website_url_from_config(config_file="config.ini"):
+def get_website_url_from_config(config_file="config.yaml"):
     config = configparser.ConfigParser()
     config.read(config_file)
     return config.get("Settings", "website_url")
@@ -28,4 +43,6 @@ def get_website_url_from_config(config_file="config.ini"):
 if __name__ == "__main__":
     website_url = get_website_url_from_config()
     generate_qr_code(website_url)
-    print(f"QR code for {website_url} QrCode Generated Saved As qrcode.png.")
+    print(f"\nQR code for {website_url} Generated and Saved As qrcode.png.")
+
+print()
